397ef9d5b81b43ef68b0ceaabb26a1e481ff4904
ahsansan <93358769+ahsansan@users.noreply.github.com>
2022-01-13 20:18:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import './styles/styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import LandingPage from './pages/LandingPage'
-import Login from './component/Login';
-import Register from './component/Register';
 
 function App() {
   return (
